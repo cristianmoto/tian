bb956655e743fb16c96b773304b0246033275cf3
Cristian Aguirre <cristianmoto066@gmail.com>
2025-04-09 09:53:40 -0300

    services

--- src/app/(routes)/services/page.tsx
+++ src/app/(routes)/services/page.tsx
@@ -14,10 +14,10 @@ const Servicios = () => {
       <TransitionsPage />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+      <div className="grid items-center justify-center h-screen max-w-5xl gap-8 mx-auto md:grid-cols-2">
         <div className="max-w-[450px]">
 
-          <h1 className="mt-[150px] text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
+          <h1 className="mt-[150px] px-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
           <p className=" px-2 mb-3 text-lg text-gray-300">Me dedico al desarrollo web frontend y creo sitios y aplicaciones que no solo se ven bien, sino que también funcionan de maravilla. Trabajo con tecnologías como HTML, CSS y JavaScript para diseñar interfaces que sean fáciles de usar, se adapten a cualquier dispositivo y capturen la esencia de la marca de cada cliente. Mi objetivo es ayudar a mejorar su imagen online y destacar en el mundo digital.</p>
           
         </div>
