e90f94b0f7e4afa48fc5babcac55a8339be73e93
Cristian Aguirre <cristianmoto066@gmail.com>
2025-04-09 10:01:42 -0300

    flecha

--- src/app/components/SliderServices.tsx
+++ src/app/components/SliderServices.tsx
@@ -2,7 +2,7 @@
 import { Swiper} from 'swiper/react';
 import {SwiperSlide} from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-
+import { GoArrowBoth } from "react-icons/go";
 import { serviceData } from '../../../data';
 
 
@@ -33,9 +33,11 @@ const SliderServices = () => {
 
         {serviceData.map((item, index) => (
             <SwiperSlide key={index} >
+                 <GoArrowBoth />
                 <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
                     <div className="mb-4 text-4xl text-secondary">{item.icon}</div>
                     <div>
+                   
                         <h3 className="mb-4 text-lg">{item.title}</h3>
                         <p className="text-sm">{item.description}</p>
                     </div>
